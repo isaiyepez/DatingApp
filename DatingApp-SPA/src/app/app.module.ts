add4ca3c87a40645218dcef388914cd6b704f3c1
isaiyepez <isaiyepez@gmail.com>
2020-07-05 00:49:21 -0500

    End of section 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -20,11 +22,13 @@ import { ErrorInterceptorProvider } from './services/error.interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
-      -ErrorInterceptorProvider
+      ErrorInterceptorProvider
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { AlertifyService } from '../services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,28 +11,33 @@ export class NavComponent implements OnInit {
   model: any = {};
 
   // Injecting our Auth service inside our component constructor:
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in sucessfully');
+      this.alertify.success('Logged in sucessfully');
     }, error => {
-      console.log('Failed to login');
+      this.alertify.error('Failed to login');
     });
-    console.log(this.model);
+    // console.log(this.model);
   }
 
   loggedIn(){
-    const token = localStorage.getItem('token');
-    return !!token; // If token is true, it will return true. otherwise,  it will return false.
+     // const token = localStorage.getItem('token');
+     // return !!token; // If token is true, it will return true. otherwise,  it will return false.
+
+    // We substituted the code above for this new version since we are now using our angular
+    // jwt validator, and it should be added as a service.
+
+     return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
