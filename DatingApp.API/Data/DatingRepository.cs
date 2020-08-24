a1eb4ece91dcd0e231de6ef8427014f00e2ce626
isaiyepez <isaiyepez@gmail.com>
2020-08-23 23:16:38 -0500

    Progress with section 14

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using DatingApp.API.Models;
 using System.Collections.Generic;
 using System.Linq;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API.Data
 {
@@ -43,11 +44,11 @@ namespace DatingApp.API.Data
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+            var users = _context.Users.Include(p => p.Photos);
 
-            return users;
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
